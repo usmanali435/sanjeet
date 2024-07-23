457cb02d61f75fedf547908194387729f72c4066
Tahir <tahirunity786@gmail.com>
2024-07-23 18:39:16 +0500

    Add analyzer page

--- js/worker.js
+++ js/worker.js
@@ -78,3 +78,11 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 });
 
+window.addEventListener('scroll', function() {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('navbar-scrolled');
+    } else {
+        navbar.classList.remove('navbar-scrolled');
+    }
+});
